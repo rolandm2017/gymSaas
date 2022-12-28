02c8b1cfa3c29def4b476b311ba10c4665f701bc
Roland Mackintosh <rolandmackintosh@outlook.com>
2022-12-27 17:58:42 -0800

    test decorator throwing

--- tests/util/decorator.test.ts
+++ tests/util/decorator.test.ts
@@ -19,12 +19,14 @@ describe("the try/catch decorators", () => {
         }
 
         const objToTest = new SomeTestObject();
+        // act
         objToTest.throwStuff();
         objToTest.throwMoreStuff();
+        // assert
         const expectedError = new Error(errMsgOne);
-        const expectedError2 = new Error(errMsgTwo);
+        const expectedErrorTwo = new Error(errMsgTwo);
         expect(console.log).toBeCalledWith({}, expectedError);
-        expect(console.log).toBeCalledWith({}, expectedError2);
+        expect(console.log).toBeCalledWith({}, expectedErrorTwo);
     });
     test("the method decorator works", () => {
         // arrange
@@ -44,11 +46,68 @@ describe("the try/catch decorators", () => {
             }
         }
         const objToTest = new AnotherTestObject();
+        // act
         objToTest.throwStuff();
         objToTest.throwMoreStuff();
+        // assert
         const expectedError = new Error(errMsgOne);
-        const expectedError2 = new Error(errMsgTwo);
+        const expectedErrorTwo = new Error(errMsgTwo);
         expect(console.log).toBeCalledWith({}, expectedError);
-        expect(console.log).toBeCalledWith({}, expectedError2);
+        expect(console.log).toBeCalledWith({}, expectedErrorTwo);
+    });
+    test("you can throw in the class decorator handler func", () => {
+        // arrange
+        const errMsgOne = "Something went wrong!";
+        const errMsgTwo = "Not intended error";
+
+        @TryCatchClassDecorator(Error, (err, context) => {
+            throw err;
+        })
+        class YeOleTestObject {
+            throwSomeStuff() {
+                throw new Error(errMsgOne);
+            }
+
+            yeOleThrower() {
+                throw new Error(errMsgTwo);
+            }
+        }
+
+        const objToTest = new YeOleTestObject();
+        // act, assert
+        const expectedError = new Error(errMsgOne);
+        const expectedErrorTwo = new Error(errMsgTwo);
+
+        // thrower();
+        expect(objToTest.throwSomeStuff).toThrow(expectedError);
+        expect(objToTest.yeOleThrower).toThrow(expectedErrorTwo);
+    });
+    test("you can throw in the method decorator handler func", () => {
+        // arrange
+        console.log = jest.fn();
+        const errMsgTwo = "A problem occurred!";
+        const errMsgThree = "This is unexpected.";
+
+        class ThyTestObject {
+            // @TryCatchMethodDecorator(Error, (err, context) => console.log(context, err))
+            @TryCatchMethodDecorator(Error, (err, context) => {
+                throw err;
+            })
+            throwOtherStuff() {
+                throw new Error(errMsgTwo);
+            }
+
+            throwMoreStuff() {
+                throw new Error(errMsgThree);
+            }
+        }
+
+        const objToTestTwo = new ThyTestObject();
+        // act, assert
+        const expectedErrorTwo = new Error(errMsgTwo);
+        const expectedErrorThree = new Error(errMsgThree);
+
+        expect(objToTestTwo.throwOtherStuff).toThrow(expectedErrorTwo);
+        expect(objToTestTwo.throwMoreStuff).toThrow(expectedErrorThree);
     });
 });
