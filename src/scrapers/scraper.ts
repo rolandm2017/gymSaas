cfce2b555e21502a449b3521c8fe3602e9e00059
Roland Mackintosh <rolandmackintosh@outlook.com>
2022-12-27 21:36:30 -0800

    clean up

--- src/scrapers/scraper.ts
+++ src/scrapers/scraper.ts
@@ -1,9 +1,6 @@
 import axios, { AxiosResponse } from "axios";
-import TaskDAO from "../database/dao/task.dao";
-import { TaskCreationAttributes } from "../database/models/Task";
 import { ProviderEnum } from "../enum/provider.enum";
 import { IHousing } from "../interface/Housing.interface";
-import { ILatLong } from "../interface/LatLong.interface";
 import Parser from "../util/parser";
 
 // class will handle info about various scraper types. "is it a long/lat or city input? does it return aps with streets, coords, both?"
@@ -11,13 +8,11 @@ class Scraper {
     site: string;
     ip: string;
     port: number;
-    private taskDAO: TaskDAO;
 
-    constructor(site: string, ip: string, port: number, taskDAO: TaskDAO) {
+    constructor(site: string, ip: string, port: number) {
         this.site = site;
         this.ip = ip;
         this.port = port;
-        this.taskDAO = taskDAO;
     }
 
     async scrape(lat: number, long: number, provider: ProviderEnum): Promise<IHousing[]> {
@@ -33,26 +28,6 @@ class Scraper {
         const housingData: IHousing[] = parser.parse(results.data);
         return housingData;
     }
-
-    // async queueGridScrape(tasks: ILatLong[], zoomWidth: number, batchNum: number): Promise<boolean> {
-    //     try {
-    //         // todo: go into shared db and queue tasks.
-    //         for (const task of tasks) {
-    //             const newTask: TaskCreationAttributes = {
-    //                 ...task,
-    //                 zoomWidth: zoomWidth,
-    //                 lastScan: null,
-    //                 batchId: batchNum,
-    //                 providerName: ProviderEnum.rentCanada, // TODO: distribute tasks to all 3 providers when ready
-    //             };
-    //             await this.taskDAO.createTask(newTask);
-    //         }
-    //         return true;
-    //     } catch (err) {
-    //         console.log(err);
-    //         return false;
-    //     }
-    // }
 }
 
 export default Scraper;
