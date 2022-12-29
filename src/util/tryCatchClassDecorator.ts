ba739eced9129f89cd201ef89d75ef15cbd8d8ac
Roland Mackintosh <rolandmackintosh@outlook.com>
2022-12-28 22:02:17 -0800

    cleanup console logs

--- src/util/tryCatchClassDecorator.ts
+++ src/util/tryCatchClassDecorator.ts
@@ -4,29 +4,16 @@ type HandlerFunction = (error: any, ctx: any) => void;
 
 // Decorator factory function // previously called Catch
 export const TryCatchClassDecorator = (errorType: any, handler: HandlerFunction): any => {
-    console.log("decraoting", errorType, "9rm");
     return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
         // Method decorator
-        // console.log(target, Reflect.ownKeys(target.prototype), Object.getOwnPropertyNames(target.prototype), "12rm");
         if (descriptor) {
             return _generateDescriptor(descriptor, errorType, handler);
         }
         // Class decorator
         else {
-            console.log(target, target.constructor, "17rm");
-            console.log(target.prototype, "18rm"); // fixme: target.prototype is empty! // fails!
-            console.log(Object.getOwnPropertyNames(target), "19rm");
-            console.log(Reflect.ownKeys(target.prototype), "20rm");
-            console.log(
-                Reflect.ownKeys(target.prototype).filter(prop => prop !== "constructor"),
-                "23rm",
-            );
-            console.log(Object.getPrototypeOf(target), "27rm");
             // Iterate over class properties except constructor
             for (const propertyName of Reflect.ownKeys(target.prototype).filter(prop => prop !== "constructor")) {
-                console.log(propertyName, "21rm");
                 const desc = Object.getOwnPropertyDescriptor(target.prototype, propertyName)!;
-                console.log(desc, errorType, "19rm");
                 const isMethod = desc.value instanceof Function;
                 if (!isMethod) continue;
                 Object.defineProperty(target.prototype, propertyName, _generateDescriptor(desc, errorType, handler));
@@ -39,11 +26,9 @@ function _generateDescriptor(descriptor: PropertyDescriptor, errorType: any, han
     // Save a reference to the original method
     const originalMethod = descriptor.value;
 
-    console.log("here 33rm");
     // Rewrite original method with try/catch wrapper
     descriptor.value = function (...args: any[]) {
         try {
-            console.log(args, "32rm");
             const result = originalMethod.apply(this, args);
 
             // Check if method is asynchronous
