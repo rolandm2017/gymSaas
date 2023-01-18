8d07e91ee918047038a5340926717dfc98643b67
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-01-18 10:06:15 -0800

    improve error handler

--- src/util/handleErrorResponse.ts
+++ src/util/handleErrorResponse.ts
@@ -4,8 +4,9 @@ export function handleErrorResponse(response: Response, error: unknown) {
     // 2 benefits of using this: (1) it's always "error" never "err" or "msg". (2) lesst yping
     console.log(error, "5rm");
     if (error instanceof Error) {
-        console.log(error.name, error.message, "6rm");
-        return response.status(400).json(error);
+        console.log(error.name, "7rm");
+        console.log(error.message, "8rm");
+        return response.status(400).json({ status: "Error", name: error.name, message: error.message });
     }
     return response.status(400).json(error);
 }
