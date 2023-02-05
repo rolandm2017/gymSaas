e9b8e24f2e23185e7d59182327d3ffe610d98683
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-02-05 10:11:23 -0800

    change success redirect

--- src/middleware/passport.middleware.ts
+++ src/middleware/passport.middleware.ts
@@ -1,6 +1,7 @@
 import passport from "passport";
+import { getFrontendURL } from "../util/URLMaker";
 
-export const googleAuth = passport.authenticate("google", { scope: ["email", "profile"], successRedirect: "http://localhost:3000" });
+export const googleAuth = passport.authenticate("google", { scope: ["email", "profile"], successRedirect: getFrontendURL("/dashboard") });
 
 export const googleAuthCallback = passport.authenticate("google", { session: false });
 
