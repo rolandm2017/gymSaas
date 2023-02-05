415427c5cacfe6848213ef932910446cf5fdd1f2
Roland Mackintosh <rolandmackintosh@outlook.com>
2023-02-05 10:11:43 -0800

    rebuild

--- dist/middleware/passport.middleware.js
+++ dist/middleware/passport.middleware.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.passportJwt = exports.googleAuthCallback = exports.googleAuth = void 0;
 const passport_1 = __importDefault(require("passport"));
-exports.googleAuth = passport_1.default.authenticate("google", { scope: ["email", "profile"], successRedirect: "http://localhost:3000" });
+const URLMaker_1 = require("../util/URLMaker");
+exports.googleAuth = passport_1.default.authenticate("google", { scope: ["email", "profile"], successRedirect: (0, URLMaker_1.getFrontendURL)("/dashboard") });
 exports.googleAuthCallback = passport_1.default.authenticate("google", { session: false });
 exports.passportJwt = passport_1.default.authenticate("jwt", { session: false });
